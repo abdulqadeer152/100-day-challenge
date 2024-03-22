bb48ff4a6eefd9d94e1f370f2373e49283d5d37b
abdulqadeer152 <160298205+abdulqadeer152@users.noreply.github.com>
2024-03-22 18:38:44 +0500

    Add files via upload

---
+++ city_nam.js
@@ -0,0 +1,5 @@
+"use strict";
+function city_country(city, Country) {
+    return `${city}, ${Country}`;
+}
+console.log(city_country('karachi', 'pakistan'));
---
+++ city_nam.ts
@@ -0,0 +1,8 @@
+
+function city_country(city:string, Country:string){
+
+    return `${city}, ${Country}`
+}
+
+console.log(city_country('karachi', 'pakistan'));
+
